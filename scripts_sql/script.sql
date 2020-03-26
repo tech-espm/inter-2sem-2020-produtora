ff03600b220e0321c3f5ed1c7d56eaa091bb47da
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2020-03-26 09:05:14 -0300

    Criando um exemplo de api

--- scripts_sql/script.sql
+++ scripts_sql/script.sql
@@ -1,5 +1,11 @@
-CREATE DATABASE IF NOT EXISTS sk8spot;
-USE sk8spot;
+CREATE DATABASE IF NOT EXISTS produtora;
+USE produtora;
 
 -- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'
 
+CREATE TABLE tipo_local (
+  id int NOT NULL AUTO_INCREMENT,
+  nome varchar(50) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY nome_UN (nome)
+);
